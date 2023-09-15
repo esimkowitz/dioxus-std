db215260c4e84e90c851856cd27b875c0242ba82
Evan Simkowitz <esimkowitz@wustl.edu>
2023-09-14 17:57:18 -0700

    save

--- src/cached_states/mod.rs
+++ src/cached_states/mod.rs
@@ -1,34 +1,52 @@
 use dioxus::prelude::*;
+use dioxus_router::prelude::*;
 use dioxus_signals::Signal;
 use once_cell::sync::Lazy;
 use serde::{de::DeserializeOwned, Serialize};
-use std::{collections::HashMap, sync::Mutex};
+use std::{vec::Vec, sync::{atomic::{AtomicUsize, Ordering::SeqCst}, Mutex}, panic, any::Any};
 
-static SIGNAL_CACHE: Lazy<Mutex<HashMap<String, String>>> =
-    Lazy::new(|| Mutex::new(HashMap::new()));
+static SIGNAL_STACK: Lazy<Mutex<Vec<String>>> = Lazy::new(|| Mutex::new(Vec::new()));
+
+static SIGNAL_STACK_CURSOR: Lazy<Mutex<usize>> = Lazy::new(|| Mutex::new(0));
 
 pub fn use_cached_signal<T: 'static + Serialize + DeserializeOwned>(
     cx: &ScopeState,
     f: impl FnOnce() -> T,
 ) -> Signal<T> {
-    let scope_name = cx.name();
-    let key = format!("signal-{}{}", scope_name, &std::any::type_name::<T>());
-    log::info!("Key: {}", key);
+    log::info!("Scope ID: {}, Scope generation: {}", cx.scope_id().0, cx.generation());
+    log::info!("Signal stack size before: {}", SIGNAL_STACK.lock().unwrap().len());
     let hook = *cx.use_hook(|| {
-        if let Some(signal) = SIGNAL_CACHE.lock().unwrap().get(&key.clone()) {
-            if let Ok(signal) = serde_json::from_str::<Signal<T>>(signal) {
-                return signal;
+        let mut signal_stack = SIGNAL_STACK.lock().unwrap();
+        let mut cursor = SIGNAL_STACK_CURSOR.lock().unwrap();
+        
+        let signal = {
+            if signal_stack.len() > *cursor {
+                serde_json::from_str::<Signal<T>>(&signal_stack[*cursor]).map_or_else(|_| Signal::new(f()), |s| s)
+            } else {
+                let init_val = f();
+                signal_stack.push(serde_json::to_string(&init_val).unwrap());
+                Signal::new(init_val)
             }
-        }
-        Signal::new(f())
+        };
+        *cursor += 1;
+        signal
     });
 
-    hook.with(|val| {
-        SIGNAL_CACHE
-            .lock()
-            .unwrap()
-            .insert(key, serde_json::to_string(val).unwrap())
-    });
+    if cx.generation() == 0 {
+        return hook;
+    }
+    else {
+        let mut signal_stack = SIGNAL_STACK.lock().unwrap();
+        let mut cursor = SIGNAL_STACK_CURSOR.lock().unwrap();
+        log::info!("gen!=0; cursor: {}, Signal stack size: {}", cursor, signal_stack.len());
+        if *cursor >= signal_stack.len() - 1 {
+            *cursor += 1;
+        } else {
+            *cursor = 0;
+        }
+        log::info!("gen!=0; cursor to use: {}", cursor);
+        signal_stack[*cursor] = serde_json::to_string(&hook).unwrap();
+    }
 
     hook
 }
